f6e302a7b0004aa66f48bd602fc43e1eb3dbdfbb
revindaamalias <revindaamalia@gmail.com>
2020-09-17 09:03:23 +0700

    layout 1

--- app/src/main/java/id/putraprima/mobile2020starterlayout02/MainActivity.java
+++ app/src/main/java/id/putraprima/mobile2020starterlayout02/MainActivity.java
@@ -3,6 +3,9 @@ package id.putraprima.mobile2020starterlayout02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.btnPushup);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, PushUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/id/putraprima/mobile2020starterlayout02/PushUpActivity.java
+++ app/src/main/java/id/putraprima/mobile2020starterlayout02/PushUpActivity.java
@@ -3,12 +3,29 @@ package id.putraprima.mobile2020starterlayout02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PushUpActivity extends AppCompatActivity {
+    private int mCounter = 0;
+    Button btn;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_push_up);
+
+        btn = (Button) findViewById(R.id.btnPush);
+        txt = (TextView) findViewById(R.id.txtPushupCount);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                mCounter ++;
+                txt.setText(Integer.toString(mCounter));
+            }        });
     }
 }
